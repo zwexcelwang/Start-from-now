f13bceeaff0ae0ba82c6543e53e27afe72243d3b
zwexcelwang <zwexcelwangwang@163.com>
2022-07-17 22:56:59 +0800

    springboot pro

---
+++ Java/21-Reggie/src/main/java/com/example/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理
+ */
+@Slf4j
+@ResponseBody
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+public class GlobalExceptionHandler {
+
+    /**
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+
+        return R.error("未知错误");
+    }
+
+}
